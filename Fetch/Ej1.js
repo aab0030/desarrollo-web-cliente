e796ba210b1604933b73f450d834948a9b335839
aab0030 <aab0030@alu.medac.es>
2025-02-06 12:39:02 +0100

    dia 06/02 trabajo

---
+++ Fetch/Ej1.js
@@ -0,0 +1,28 @@
+document.querySelector("button").addEventListener("click", function () {
+    let salida = document.querySelector("#salida");
+    const url = "https://jsonplaceholder.typicode.com/comments";//url de nuestra bbdd 
+  
+    fetch(url)
+      .then(function (respuesta) {
+        if (!respuesta.ok) {
+          throw new Error("Error del Fetch: " + respuesta.status);
+        }
+        return respuesta.json();
+      })
+  
+      .then(function (datos) {
+        datos.forEach(dato => {
+            
+            salida.textContent += dato.body + '\n';
+            
+        });
+      })
+      //para que haya error
+      .catch(function (error) {
+        alert("Problemas accediendo a la URL:" + error);
+      });
+  });
+  
+  
+  
+  
